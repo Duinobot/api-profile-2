1ca720cee3e7a5d20c7f9eb62bd348b74a60e5a6
Superdan <danmo2060@gmail.com>
2020-07-10 15:06:04 +1000

    first migrations'

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,15 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-class UserProfile(AbstractUser, PermissionsMixin):
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user porofiles"""
+    
+    def create_user(self, email, name, password=None):
+        """To new create user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, name, password):
+        """To create a superuser"""
+        user = self.create_user(email, name, password)
+        
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+        
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     
-    objects = UserProfileManger()
+    objects = UserProfileManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
