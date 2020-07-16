cb6121378318a4993b67ab05db1b0d9fef6c0dd2
Superdan <danmo2060@gmail.com>
2020-07-16 14:25:24 +1000

    added login api to views

--- profile_api/views.py
+++ profile_api/views.py
@@ -4,10 +4,14 @@ from rest_framework import status #return HTTP responses
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.settings import api_settings
 
 from profile_api import serializers
 from profile_api import models
 from profile_api import permissions
+
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -99,3 +103,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name","email",)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
